e5ee6c34c47fcee1c70b40e415b2e99b600a653f
saitarun135 <saitarun135@gm.com>
2021-11-05 14:27:38 +0530

    UC-3:- OPTIONS[Ladder | snake | no-play] added.

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -4,10 +4,31 @@ public class snake_and_ladder{
 
       /* TEMPORARY VARIABLES */
       int player_position = 0;
+
+		/* CONSTANT VARIABLES */
+      final int ladder = 2;
+      final int snake = 1;
+
       System.out.println("PLAYER POSITION AT START :- " + player_position);
 
 		int die = (int)Math.floor(Math.random()*6+1);
+
+		/* UC-3 :- OPTIONS */
+      int options = (int)Math.floor(Math.random()*3);
+
       System.out.println("DIE ROLLED AND VALUE IS " + die + ".\n");
+		switch(options){
+         case ladder :
+            player_position += die;
+            System.out.println("\nPLAYER CLIMBED LADDER WITH " + die + " .");
+            break;
+         case snake :
+            player_position -= die;
+            System.out.println("\nPLAYER BITTEN BY A SNAKE WITH " + die + " .");
+            break;
+         default :
+            System.out.println("\nPLAYER CHOOSED NO PLAY OPTION.");
+      }
    }
 }
 
