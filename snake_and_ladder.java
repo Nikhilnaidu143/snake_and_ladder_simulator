5cee6499c01e9d900aca0998f015b6da4385a558
saitarun135 <saitarun135@gm.com>
2021-11-05 13:28:42 +0530

    UC-1:- Initial position of player is 0 added.

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -1,5 +1,10 @@
 public class snake_and_ladder{
    public static void main(String[] args){
       System.out.println("WELCOME TO SNAKE & LADDER SIMULATOR...\n\n");
+
+      /* TEMPORARY VARIABLES */
+      int player_position = 0;
+      System.out.println("PLAYER POSITION AT START :- " + player_position);
    }
 }
+
